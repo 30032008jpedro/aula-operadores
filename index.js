67c07c964d4ffe4b597ae5f308bee4997a23b7d2
Joao Pedro <jotappe.domingues05@gmail.com>
2023-04-12 21:46:04 -0300

    Exercicios de operados

---
+++ index.js
@@ -0,0 +1,22 @@
+//calcule e mostre o valor de 77 F em K
+
+//(Kelvin) = (Graus_Fahreinheit -32)*(5/9) +273.15
+
+ (Kelvin) = (77- 32 )* (5/9) +273.15
+console.log(Kelvin);
+
+ (graus_Fahreinhet) = (80)*(9/5) + 32
+console.log(graus_Fahreinhet);
+
+//a
+Kelvin=null
+graus_Fahreinhet =null
+celsius= null
+ 
+graus_Fahreinhet = parseFloat(prompt ("Insira a unidade F que será convertida em K"))
+
+Kelvin (graus_Fahreinhet+273.15);
+console.log(Kelvin ("K")) ;
+
+// d) Altere o ultimo  item para que o usuario insira o valor em graus Celsius que ele deseja converter 
+
